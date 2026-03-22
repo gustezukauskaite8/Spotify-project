52af7ba844f8f441ed84e09a85ed7af674ae6b0d
miaengela-bit <mia.engela@gmail.com>
2026-03-22 14:28:58 +0100

    Refactor comments and remove print statements
    Updated comments for clarity and removed print statements related to the cleaning process.

--- Cleaning_all.py
+++ Cleaning_all.py
@@ -10,10 +10,9 @@ from itertools import combinations
 ##########################################################################################################################
 # Mia
 def create_cleaned_database(source_db, target_db):
-    print("🚀 Starting data cleaning process...")
     conn = sqlite3.connect(source_db)
     
-    # 1. CLEAN ARTISTS
+    # clean artists
     df_artists = pd.read_sql_query("SELECT * FROM artist_data", conn)
     df_artists.replace('', np.nan, inplace=True)
     df_artists = df_artists.dropna(subset=['id', 'name', 'genre_0'])
@@ -23,21 +22,21 @@ def create_cleaned_database(source_db, target_db):
     df_artists = df_artists.sort_values(by=['name_clean', 'artist_popularity', 'followers'], 
                                         ascending=[True, False, False])
     
-    # Create the master cleaned artist list
+    # Create the cleaned artist list
     df_artists_cleaned = df_artists.drop_duplicates(subset=['name_clean'], keep='first').copy()
     
     # Create the mapping dictionary for ID consistency
     name_to_master_id = dict(zip(df_artists_cleaned['name_clean'], df_artists_cleaned['id']))
     mapping_dict = dict(zip(df_artists['id'], df_artists['name_clean'].map(name_to_master_id)))
 
-    # 2. CLEAN ALBUMS 
+    # Albums cleaning
     df_albums = pd.read_sql_query("SELECT * FROM albums_data", conn)
     df_albums.replace('', np.nan, inplace=True)
     df_albums['artist_id'] = df_albums['artist_id'].map(mapping_dict)
     df_albums = df_albums.dropna(subset=['track_id', 'album_id', 'artist_id'])
     df_albums = df_albums[(df_albums['duration_sec'] > 0) & (df_albums['duration_ms'] > 0)]
 
-    # 3. CLEAN TRACKS & FEATURES
+    # Tracks adn features 
     df_tracks = pd.read_sql_query("SELECT * FROM tracks_data", conn)
     df_features = pd.read_sql_query("SELECT * FROM features_data", conn)
     
@@ -50,8 +49,7 @@ def create_cleaned_database(source_db, target_db):
     
     conn.close()
 
-    # --- SAVE TO NEW DATABASE ---
-    print(f"💾 Saving cleaned data to {target_db}...")
+    # New database
     clean_conn = sqlite3.connect(target_db)
     
     df_artists_cleaned.to_sql('artists', clean_conn, if_exists='replace', index=False)
@@ -60,7 +58,6 @@ def create_cleaned_database(source_db, target_db):
     df_features.to_sql('features', clean_conn, if_exists='replace', index=False)
     
     clean_conn.close()
-    print("✅ Done! Your dashboard can now use the cleaned database.")
 
 if __name__ == "__main__":
     create_cleaned_database('data/spotify_database.db', 'data/spotify_clean.db')
